00058e2208ea01e50065637a137d3d4dec82dff5
Vicente Maroto <vicente.maroto@escuelaestech.es>
2021-01-28 21:11:46 +0100

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by A4-iMAC01 on 28/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

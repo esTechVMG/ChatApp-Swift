9605824ba64dae8219dcafc0fdf82fcc58c3bfb2
Vicente Maroto <vicente.maroto@escuelaestech.es>
2021-02-03 18:49:00 +0100

    Add Apn Object and UserMessage convertor

--- ChatApp/User.swift
+++ ChatApp/User.swift
@@ -21,11 +21,55 @@ struct UserList:Codable {
     }
 }
 struct UserMessage {
-    var author:String = "No Author"
-    var message:String = "Hello World"
-    var isMessageMine:Bool = true
+    var author:String? = "UnknownUser"
+    var message:String = "HelloWorld"
+    var isMessageMine:Bool?
+    init(author:String?, message:String) {
+        self.author = author
+        self.message = message
+    }
 }
 struct  UserMessageList{
     var messageList:[UserMessage]
+    init() {
+        messageList=Array()
+    }
 }
 
+func apnObjectToUserMessageObject(apn:ApnObject) -> UserMessage {
+    return UserMessage(author: apn.username, message: apn.aps.alert.body)
+}
+
+/*
+ APNS Message
+ {
+     "aps":{
+         "alert": {
+             "title": "Notificación recibida de la aplicacion push",
+             "body": "Acepta el nuevo evento para mas informacion"
+         },
+         "sound": "default",
+         "badge": 3,
+         "link_url": "https://escuelaestech.es",
+         "category": "TEST_PUSH"
+     },
+     "username": "Vicente"
+ }
+
+ */
+struct ApnObject {
+    var aps:aps
+    var username:String
+    var ismine:Bool
+}
+struct aps {
+    var alert:alert
+    var sound:String
+    var badge:Int
+    var link_url:String
+    var category:String
+}
+struct alert {
+    var title:String
+    var body:String
+}
